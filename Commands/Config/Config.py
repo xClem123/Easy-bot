099ee814e4b3419920ffad77ce623269d30c935c
xClem <clemleboss28+1@gmail.com>
2024-10-22 21:06:45 +0200

    Update Config.py

--- Commands/Config/Config.py
+++ Commands/Config/Config.py
@@ -3,7 +3,7 @@ PREFIX = "&" # PREFIX BOT
 CREATOR_ID = 1203741606965149698 # YOUR ID DISCORD
 EMBED_COLOR = "a80505"
 
-discord_server = "discord.gg/server-boost"
+discord_server = "discord.gg/datahub"
 website = "SOON"
 telegram = "SOON"
-github_tool = "github.com/beatch-service"
+github_tool = "github.com/xClem123"
